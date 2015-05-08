2490f31e08f45dcdbf099cecb138c5fe4f3feec1
David Rogers <al.the.x@gmail.com>
2015-05-08 14:18:22 -0400

    Starting point

---
+++ 04--TodoCLI-Part-2/check-writing.js
@@ -0,0 +1,19 @@
+/**
+ * When you write a check, to validate the amount,
+ * you have to write the English words for the amount
+ * on a separate line. For example, a check for $123.45
+ * has a line with:
+ *
+ *    one hundred twenty three and 45/100s dollars
+ *
+ * Given a function `toEnglish` that accepts a JavaScript
+ * `Number` and returns a `String` formatted for use on a
+ * check, as above.
+ */
+var expect = require('chai').expect;
+
+function toEnglish(number){
+    // ????
+}
+
+// Test cases here...
