50cba3d392b7dfc7fe1dc4b6c0834314d1001cf1
David Rogers <al.the.x@gmail.com>
2015-05-11 15:33:23 -0400

    Finishing off "completed"...

--- 05--Monday-Review+Preview/todo.js
+++ 05--Monday-Review+Preview/todo.js
@@ -20,16 +20,29 @@ function addTaskToList(task, list){
     // add task to list somehow on the bottom
 }
 
+function completeTaskByIndex(index, list){
+    // Complete the task at `index` in `list`
+    list[index].completed = true;
+}
+
 expect(taskList.length).to.equal(0);
 addTaskToList("Remember the milk", taskList);
 expect(taskList.length).to.equal(1);
 expect(taskList[0].text)
     .to.equal("Remember the milk");
+expect(taskList[0].completed)
+    .to.be.false;
 
 expect(taskList.length).to.equal(1);
 addTaskToList("Take out the trash", taskList);
 expect(taskList.length).to.equal(2);
 expect(taskList[1].text)
     .to.equal("Take out the trash");
+expect(taskList[1].completed)
+    .to.be.false;
 
-// expect(taskList[1]).to.not.be.completed?
+expect(taskList[1].completed)
+    .to.be.false;
+completeTaskByIndex(1, taskList);
+expect(taskList[1].completed)
+    .to.be.true;
